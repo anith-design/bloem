8311ddc14e933f666949847087acfcf2fef4ebc6
Anith Vishwanath <11884278+anithvishwanath@users.noreply.github.com>
2024-11-03 21:43:20 +0530

    Added riso Colors

--- sketches/2024.11.03-10.35.35.js
+++ sketches/2024.11.03-10.35.35.js
@@ -1,19 +1,20 @@
 const canvasSketch = require("canvas-sketch");
-const risoColors = require("riso-colors");
+const risoColors = require("../assets/risoColors.json");
 
 const settings = {
   dimensions: [2048, 2048],
   pixelsPerInch: 300,
   animate: true,
-  duration: 12,
+  duration: 16,
   fps: 60
 };
 
 const sketch = () => {
+  const goldenRatio = (1 + Math.sqrt(5)) / 2;
   const color = risoColors[Math.floor(Math.random() * risoColors.length)];
-  return ({ context, width, height, time }) => {
+  return ({ context, width, height, playhead }) => {
     const margin = 120;
-    const cols = 40;
+    const cols = 30;
     const rows = cols;
 
     const gridWidth = width - 2 * margin;
@@ -45,14 +46,15 @@ const sketch = () => {
         const distance = Math.sqrt(distX * distX + distY * distY);
 
         //const wave = Math.sin(distance - time * 1.5);
-        const wave = Math.sin(distance - time * 1.5);
+        const wave = Math.sin(distance * goldenRatio - playhead * (Math.PI * 2 * 3));
         //const wave = Math.sin(distance * 0.05 - time * 5)
-
+        
         const scale = (wave + 1) / 2;
 
         context.save();
 
         context.translate(gridCenterX, gridCenterY);
+        // context.rotate(wave * Math.PI * 1);
         context.scale(scale, scale);
 
         context.fillStyle = color.hex;
