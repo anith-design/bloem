c6121f3b3d09d529d8b9a25320b15881687e6b2d
Anith Vishwanath <11884278+anithvishwanath@users.noreply.github.com>
2024-11-03 12:01:18 +0530

    Initial commit

---
+++ sketches/2024.11.03-10.35.35.js
@@ -0,0 +1,67 @@
+const canvasSketch = require("canvas-sketch");
+const risoColors = require("riso-colors");
+
+const settings = {
+  dimensions: [2048, 2048],
+  pixelsPerInch: 300,
+  animate: true,
+};
+
+const sketch = () => {
+  const color = risoColors[Math.floor(Math.random() * risoColors.length)];
+  return ({ context, width, height, time }) => {
+    const margin = 120;
+    const cols = 40;
+    const rows = cols;
+
+    const gridWidth = width - 2 * margin;
+    const gridHeight = height - 2 * margin;
+
+    const squareW = gridWidth / cols;
+    const squareH = gridHeight / rows;
+
+    const startX = margin;
+    const startY = margin;
+
+    context.fillStyle = "hsl(0, 0%, 98%)";
+    //context.fillStyle = color.hex;
+    context.fillRect(0, 0, width, height);
+
+    const centerX = width / 2;
+    const centerY = height / 2;
+
+    for (let i = 0; i < cols; i++) {
+      for (let j = 0; j < rows; j++) {
+        const x = startX + i * squareW;
+        const y = startY + j * squareH;
+
+        const gridCenterX = x + squareW / 2;
+        const gridCenterY = y + squareH / 2;
+
+        const distX = gridCenterX - centerX;
+        const distY = gridCenterY - centerY;
+        const distance = Math.sqrt(distX * distX + distY * distY);
+
+        const wave = Math.sin(distance * 5 - time * 1.5);
+        //const wave = Math.sin(distance * 0.05 - time * 5)
+
+        const scale = (wave + 1) / 2;
+
+        context.save();
+
+        context.translate(gridCenterX, gridCenterY);
+        context.scale(scale, scale);
+
+        context.fillStyle = color.hex;
+
+        context.beginPath();
+        context.rect(-squareW / 2, -squareH / 2, squareW, squareH);
+        context.fill();
+
+        context.restore();
+      }
+    }
+  };
+};
+
+canvasSketch(sketch, settings);
